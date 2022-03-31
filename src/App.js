40935453377b57d4331401032cfa99c4f58f5926
kpcn <channyein.k@gmail.com>
2022-03-31 10:17:25 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="grid min-h-screen justify-center items-center">
+      <h1 className="text-xl text-gray-700">Coin Stats</h1>
+    </div>
+  );
+}
+
+export default App;
